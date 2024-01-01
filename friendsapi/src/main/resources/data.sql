991b0867b5aee8a53d44c00a21f8857974a2179c
venkat <naresh3456@gmail.com>
2024-01-01 11:08:15 -0500

    backend code

---
+++ friendsapi/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Friend(id,firstName,lastName,department,email,country) values(1,'naresh','K', 'CSE', 'naresh3456@gmail.com', 'usa');
+INSERT INTO Friend (id,firstName,lastName,department,email,country) values(2,'suresh','M', 'MECH', 'suresh3456@gmail.com', 'India');
+INSERT INTO Friend (id,firstName,lastName,department,email,country)values(3,'harish','A', 'IT', 'harish3456@gmail.com', 'China');
+INSERT INTO Friend (id,firstName,lastName,department,email,country) values(4,'ram','P', 'CIVIL', 'ram3456@gmail.com', 'London');
+INSERT INTO Friend (id,firstName,lastName,department,email,country) values(5,'jones','H', 'ECE', 'jones3456@gmail.com', 'Australia');
